98ebdd4034718d02e0983359f13a17fc6a0e3fb7
sohail28s <sohailzafar4u@gmail.com>
2026-06-20 14:06:16 +0500

    eeeoe

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Provider } from "./Components/ui/Provider.js"
+import { Provider } from "./Components/ui/Provider.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
