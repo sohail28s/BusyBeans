71541ebda4259b855d3f27968ab812bd6209e077
sohail28s <sohailzafar4u@gmail.com>
2026-06-20 13:36:34 +0500

    new

--- src/Components/ui/provider.jsx
+++ src/Components/ui/provider.jsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { ColorModeProvider } from './color-mode'
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Provider } from "./components/ui/provider.jsx"
+import { Provider } from "./Components/ui/Provider.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
